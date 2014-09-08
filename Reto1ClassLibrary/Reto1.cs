a89c846785473a4fc9f0d99baa82fc200484f932
jesusmateos <jesusmmateos@outlook.com>
2014-09-08 22:57:28 +0200

    Cambiado el tipo de librería a portable class library

---
+++ Reto1ClassLibrary/Reto1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reto1ClassLibrary
+{
+    public class Reto1
+    {
+        /// <summary>
+        /// Recibe una lista de personas y que la devuelve ordenada por orden descendente de edad 
+        /// (es decir, los recién nacidos aparecerían al final de la lista). 
+        /// Si dos personas tienen la misma edad, aparecerán por orden alfabético
+        /// </summary>
+        /// <returns></returns>
+        public static List<Persona> OrdenarLista(List<Persona> listaPersonas)
+        {
+            if (listaPersonas == null) return null;
+
+            var personasOrdenadas = new List<Persona> (listaPersonas);
+
+            personasOrdenadas.Sort();
+
+            return personasOrdenadas;
+        }
+
+    }
+}
